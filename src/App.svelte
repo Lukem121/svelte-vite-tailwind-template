e693de179ab1d83a3cf8f983fcdefeebb22f38ff
Lukem121 <lukeask@hotmail.co.uk>
2021-11-14 00:51:44 +0000

    updated svelte and other dependencies

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
   import './lib/TailwindCSS.svelte'
+  let increment;
 </script>
 
 <section class="text-gray-600 body-font">
@@ -10,12 +11,13 @@
     <div class="text-center lg:w-2/3 w-full">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Svelte + Vite + TailwindCSS = 🌸🌸🌸</h1>
       <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
-      <div class="mb-8">
-        <Counter id="0" />
+      <a class="my-8 bg-red-600 text-white p-4 px-8 underline rounded-3xl" href="https://github.com/Lukem121/svelte-vite-tailwind-template">Please ⭐ on GitHub</a>
+      <div class="my-8">
+        <Counter bind:increment={increment} id="0" />
       </div>
       <div class="flex justify-center">
-        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" on:click={increment}>Button</button>
+        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg" on:click={increment}>Button</button>
       </div>
     </div>
   </div>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,12 +1,10 @@
 <script>
-  import { getStore } from './hmr-stores'
-  /** @type { string } */
-  export let id
-
-  const count = getStore(id, 0)
-  const increment = () => {
+  import { getStore } from './hmr-stores';
+  export let id;
+  const count = getStore(id, 0);
+  export const increment = () => {
     $count += 1
-  }
+  };
 </script>
 
 <button class="px-8 py-4 w-52 text-red-500 bg-red-200 rounded-full" {id} on:click={increment}>
