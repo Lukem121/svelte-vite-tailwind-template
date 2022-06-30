8cb01100490e6b37c8e4c6d13fa579350579caee
Luke <lukeask@hotmail.co.uk>
2022-06-30 17:07:45 +0700

    home page styling

--- src/App.svelte
+++ src/App.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <section class="text-gray-600 body-font">
-  <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={logo}>
+  <div class="container mx-auto flex px-5 py-8 items-center justify-center flex-col">
+    <img class="lg:w-1/6 md:w-3/6 w-5/6 mb-6 object-cover object-center rounded" alt="hero" src={logo}>
     <div class="text-center lg:w-2/3 w-full">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Svelte + Vite + TailwindCSS = 🌸🌸🌸</h1>
       <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
